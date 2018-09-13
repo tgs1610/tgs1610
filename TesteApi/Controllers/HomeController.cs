a2141a9719ad144f6a32771598692f7f8335ade5
thiagogsantos <thiagogsantos@hotmail.com>
2018-09-13 00:26:42 -0300

    teste commit inicial

---
+++ TesteApi/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace TesteApi.Controllers
+{
+    public class HomeController : ApiController
+    {
+
+        public object Get()
+        {
+            return new {id=1,nome="teste" };
+        }
+
+    }
+}
